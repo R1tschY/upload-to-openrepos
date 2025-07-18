9589792c3496eb8712b48fa6a430e7c44877bab4
Richard Liebscher <r1tschy@posteo.de>
2025-07-18 18:38:29 +0200

    Log status

--- index.js
+++ index.js
@@ -27,20 +27,36 @@ if (dryRun) {
   });
   const context = await browser.newContext();
   const page = await context.newPage();
+  console.info('📦 Opening webpage ...');
   await page.goto('https://openrepos.net/');
+
+  console.info('🔑 Login ...');
   await page.getByRole('textbox', { name: 'Username or e-mail *' }).fill(login);
   await page.getByRole('textbox', { name: 'Password *' }).fill(password);
   await page.getByRole('button', { name: 'Log in' }).click();
+
+  console.info('📱 Open app page ...');
   await page.goto(`https://openrepos.net/content/${login}/${appName}`);
+
+  console.info('✏️ Start editing app ...');
   await page.getByRole('link', { name: 'Edit', exact: true }).click();
+
+  console.info('📦 Uploading files ...');
   await page
     .getByRole('group', { name: 'Application versions' })
     .getByLabel('Add a new file')
     .setInputFiles(rpms);
   await page.locator('[id^="edit-field-packages-und-"][id$="-upload-button"]').click();
+
   if (!dryRun) {
+    console.info('💾 Saving changes ...');
     await page.getByRole('button', { name: 'Save' }).click();
+  } else {
+    console.info('🔎 Previewing changes ...');
+    await page.getByRole('button', { name: 'Preview' }).click();
   }
+
+  console.info('✅ Finished');
 
   // ---------------------
   await context.close();
